48ede2ac7828ff12c66f6e8f4354c59a61ece611
DaniilOlivo <kiselev.daniil.web@gmail.com>
2024-12-26 18:34:16 +0300

    Add app main for multiple pages

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from .views import BaseView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", BaseView.as_view(), name="base")
+    path("", include("main.urls")),
 ]
