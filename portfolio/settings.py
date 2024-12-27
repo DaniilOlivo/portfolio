e0c99b96715db3addd8e945d7a60118a34d8d2d2
DaniilOlivo <kiselev.daniil.web@gmail.com>
2024-12-27 03:46:03 +0300

    Shared static folder changed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "static"
+STATIC_ROOT = "staticfiles"
 STATICFILES_DIRS = (
-    path.join(BASE_DIR, "staticfiles"),
+    path.join(BASE_DIR, "static"),
 )
 
 # Default primary key field type
