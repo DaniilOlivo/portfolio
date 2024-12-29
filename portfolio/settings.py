4d3442485d7b11f033547d4e559ab9be8df06bd3
DaniilOlivo <kiselev.daniil.web@gmail.com>
2024-12-29 23:17:55 +0300

    Media setup

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = (
     path.join(BASE_DIR, "static"),
 )
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
