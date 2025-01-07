023ba38723e41ee856cb1f3c92e6293a3df2fdf3
DaniilOlivo <kiselev.daniil.web@gmail.com>
2025-01-07 05:56:00 +0300

    Creating a stat block

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from analytics.views import recieve_analytics
+from analytics.views import recieve_analytics, DashboardView
 
 urlpatterns = [
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path("time/", recieve_analytics, name="recieve_analytics")
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,7 +1,19 @@
 from analytics.models import Visit
+from django.views.generic import TemplateView
 from django.http import HttpRequest, HttpResponse
 import json
 from datetime import timedelta
+
+
+def getStatsVisits():
+    visits = Visit.objects.all()
+    count = visits.count()
+    average = sum(map(lambda v: v.time_stay, visits), timedelta()) / count
+    return {
+        "count": count,
+        "average": average - timedelta(microseconds=average.microseconds) # Убираем лишние микросекунды
+    }
+
 
 def recieve_analytics(request: HttpRequest):
     visit = Visit.objects.get(session=request.session.session_key)
@@ -12,4 +24,15 @@ def recieve_analytics(request: HttpRequest):
     visit.save()
 
     return HttpResponse("Ok")
+
+
+class DashboardView (TemplateView):
+    template_name = "analytics/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        stats = getStatsVisits()
+        context.update(stats)
+        return context
+    
     
